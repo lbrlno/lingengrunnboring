e8398ca69b90e97a7324abbb5918201b3310f329
ruben <rub1@users.noreply.github.com>
2019-02-15 15:43:21 +0100

    update
    - added instagramfeed with js library
    - added instant.page js library for prefetch
    - added another employee

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,18 +1,20 @@
-	// var navToggle = document.querySelector('.nav-toggle'),
-	// 	nav = document.querySelector('.menu');
 
-	// var classToggle = function() {
-	//   nav.classList.toggle("expanded");
-	// }
-	function toggle_visibility(id) {
-		var e = document.getElementById(id);
+function toggle_visibility(id) {
+	var e = document.getElementById(id);
 
-		if(e.style.display == 'block') {
-			e.style.display = 'none';
-		} else {
-			e.style.display = 'block';
-		}
+	if(e.style.display == 'block') {
+		e.style.display = 'none';
+	} else {
+		e.style.display = 'block';
 	}
+}
 
-	// navToggle.addEventListener('click', toggle_visibility("menu") );
-	
+var userFeed = new Instafeed({
+	get: 'user',
+	userId: '10858816759',
+	limit: 5,
+	resolution: 'low_resolution',
+	accessToken: '10858816759.e9ce625.296e76493b404c92a6cab9913809e8c5',
+	template: '<a class="instagram-item" href="{{link}}"><img src="{{image}}" width="250px" /></a>'
+});
+userFeed.run();
--- src/index.md
+++ src/index.md
@@ -8,3 +8,5 @@ Velkommen til Lingen Grunnboring AS
 
 Vi er et lite firma med høy kompetanse som har som mål å være ledende innen levering av kvalitet i levert vare.
 Vi tilbyr tjenester innen geotekniske grunnundersøkelser og har pr i dag to borerigger.
+
+{% include instagram.html %}
